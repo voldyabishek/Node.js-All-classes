8115fc1e45121abe36ce36b1dd97acbe565b21c9
voldyabishek <aa9177954@gmail.com>
2025-03-25 17:47:08 +0530

    Add files via upload

---
+++ Node.js/Node1day.js
@@ -0,0 +1,33 @@
+// first class
+
+//   console.log(global)  //one of the librarey
+
+
+  /*const os=require('os')
+  console.log(os)
+
+  console.log(os.type())
+  console.log(os.version())
+  console.log(os.homedir()) //dir mean directory*/
+    
+
+  /*console.log(__dirname)
+  console.log(__filename)*/
+
+ /* const path=require("path")
+  console.log(path.dirname(__filename))
+  console.log(path.basename(__filename))
+  console.log(path.extname(__filename))
+  console.log(path.parse(__filename))*/
+
+
+ /* const m=require('./math.js')
+  console.log(m.add(5,8));
+  console.log(m.sub(5,8));
+  console.log(m.mul(5,8))*/
+
+
+   /*const {add,sub,mul}=reqire(".math")
+  console.log(add(8,8));
+  console.log(sub(5,8));
+  console.log(mul(5,8)); /*
