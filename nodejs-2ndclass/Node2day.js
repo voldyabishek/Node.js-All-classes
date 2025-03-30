854ce51185120518e3bea7000684802959283fe7
voldyabishek <aa9177954@gmail.com>
2025-03-30 17:41:55 +0530

    Add files via upload

--- nodejs-2ndclass/Node2day.js
+++ nodejs-2ndclass/Node2day.js
@@ -1,6 +1,5 @@
 //directory creation
 //fs-file system, mkdir - make directory
-
 /* const fs = require('fs');
 fs.mkdir("abishek",(err)=>{
     if(err){
@@ -10,6 +9,9 @@ fs.mkdir("abishek",(err)=>{
     console.log('directory created successfully')
 }) */
  
+
+// <!--------------------!>
+
 //check the directory is created or not
 /* const fs = require('fs');
 fs.readdir('./abishek',(err,files)=>{
@@ -21,6 +23,8 @@ fs.readdir('./abishek',(err,files)=>{
     console.log("files in the directory");
 }) */
 
+// <!--------------------!>
+
  //check the files 
  /*  const fs = require('fs');
   const filePath ='./n.js';
@@ -30,10 +34,10 @@ fs.readdir('./abishek',(err,files)=>{
   else{
     console.log("file does not exist");
   } */
-
+// <!--------------------!>
 
     //Write and create a file
-     const fs =require('fs')
+/*      const fs =require('fs')
      const filepath ='n.js';
      const dataToWrite ='console.log("vanakam da maapala")';
      const encoding ='utf8';  //utf for website data sequireness
@@ -43,4 +47,49 @@ fs.readdir('./abishek',(err,files)=>{
             return;
         }
         console.log('Data has been written to the file.');
-     });
+     }); */
+// <!--------------------!>
+   //File Reading  
+        /* const fs =require('fs')
+        const filepath = 'n.js';
+        const encoding = 'utf8';
+        fs.readFile(filepath,encoding,(err,data)=>{
+        
+          if (err){
+            console.error(err)
+            return;
+          }
+          console.log(data);
+          console.log("ended")
+        }); */
+        /* first show this console(print) --yen ahh  mela ulla ahh code ahh run aagurathuku time aagum, 
+        athanala keela ulla code ahh first run aagodum-- example console.log mela pota sernthe run aagidum  */
+        // console.log("ended")
+
+// <!--------------------!>
+
+//file rename ---Like newfile.txt vanthu n.js nu irunthuchi keela ulla code ahh adicha antha file n.js la irunthu newfile.txt nu change aagidichi.
+   /*  const fs = require('fs');
+    const oldFilePath ='n.js';
+    const newFilepath ='newfile.txt';
+    fs.rename(oldFilePath,newFilepath,(err) =>{
+    if(err){
+    console.error(err);
+    return;
+    }
+    console.log('File renamed successfuly')
+    }) */
+// <!--------------------!>
+
+
+//file edition date and time
+const fs = require('fs');
+const filePath = './newfile.txt';
+fs.stat(filePath,(err,stats) =>{
+  if(err){
+    console.error(err);
+    return;
+  }
+  console.log('File size (in bytes):',stats.size);
+  console.log('Last modified:',stats.mttime)
+});
